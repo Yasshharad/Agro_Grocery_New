6d93bc353c37a9c25c4bfc2600d30fff5d9eb7cf
Yash harad <it.a.35.yasshharad@gmail.com>
2023-09-14 18:20:55 +0530

    changes done

--- client/src/components/Delivery/index.jsx
+++ client/src/components/Delivery/index.jsx
@@ -22,7 +22,7 @@ const Delivery = () => {
     <>
       <DeliveryCarousel />
       <h1 className="text-xl mt-4 mb-2 md:mt-8 md:text-3xl md:font-semibold">
-        Delivery Stores in Mumbai
+        Our Shops in Mumbai
       </h1>
       <div className="grid gap-0 md:gap-2 md:grid-cols-2 grid-cols-1 lg:grid-cols-3">
         {StoreList.map((Store) => (
--- client/src/pages/Checkout.page.jsx
+++ client/src/pages/Checkout.page.jsx
@@ -4,6 +4,8 @@ import { BsShieldLockFill } from "react-icons/bs";
 // Layout
 import CheckoutLayout from "../layouts/Checkout.layout";
 
+import { useDispatch } from "react-redux";
+
 // components
 import ProductItem from "../components/Cart/ProductItem";
 import AddressList from "../components/Checkout/AddressList";
@@ -11,9 +13,11 @@ import AddressList from "../components/Checkout/AddressList";
 // redux
 import { useSelector } from "react-redux";
 
-const Checkout = () => {
+const Checkout = (props) => {
   const cart = useSelector((globalState) => globalState.cart.cart);
   const user = useSelector((globalState) => globalState.user);
+
+  const dispatch = useDispatch();
 
   const address = [
     {
@@ -59,8 +63,8 @@ const Checkout = () => {
         <div className="flex w-full flex-col gap-2 items-center">
           <h5 className="text-base tracking-wider">ORDER FROM</h5>
           <div className="flex w-full flex-col items-center text-gray-400">
-            <h4>Domino's Pizza</h4>
-            <small>GT World Mall, Magadi Road, NCR Noida</small>
+            <h4>{props.name}</h4>
+            <small>{props.address}</small>
           </div>
           <div className="my-4 h-32 overflow-y-scroll px-4 flex flex-col gap-2 w-full md:w-3/5">
             {cart.map((item) => (
